e9cc841415ed12b6afa7993394729d144a194d87
ky344646 <ky344646@ucf.edu>
2026-03-08 21:26:08 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,3 +1,11 @@
+// BEFORE
+
+public static int getInteger(DataInputStream is) throws IOException {
+    return is.readInt();
+}
+
+// AFTER
+
 public static long getInteger(DataInputStream is) throws IOException {
     return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
