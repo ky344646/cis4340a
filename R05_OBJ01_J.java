081979eab0dc6323be4fbbd50dfca8c13f91ef41
ky344646 <ky344646@ucf.edu>
2026-03-08 21:36:37 -0400

    Update R05_OBJ01_J.java

--- R05_OBJ01_J.java
+++ R05_OBJ01_J.java
@@ -1,12 +1,7 @@
 // BEFORE
 
-public static int getAbsAdd(int x, int y) {
-    assert x != Integer.MIN_VALUE;
-    assert y != Integer.MIN_VALUE;
-    int absX = Math.abs(x);
-    int absY = Math.abs(y);
-    assert (absX <= Integer.MAX_VALUE - absY);
-    return absX + absY;
+public class Widget {
+    public int total;
 }
 
 // AFTER
